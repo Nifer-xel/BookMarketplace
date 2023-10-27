61c30b0ed5f12601a65f3970864ee7982745a222
Niferok <64299220+Nifer-xel@users.noreply.github.com>
2023-10-26 23:58:51 -0700

    Create IBookStore.sol

---
+++ IBookStore.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface IBookStore {
+    function makePayment(address seller, uint256 amount) external;
+    function owner() external view returns (address);
+    function ownerOf(uint256 tokenId) external view returns (address);
+    function withdraw() external;
+    function listBook(
+        string memory title,
+        uint256 priceInEth,
+        string memory coverHash,
+        string memory contentHash,
+        uint8 royaltyPercentage
+    ) external returns (uint256);
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) external;
+
+    function getRoyalty(uint256 bookId) external view returns (uint256);
+
+    function getOriginalSeller(uint256 bookId) external view returns (address);
+
+    function delist(uint256 bookId) external;
+    function getBookContent(uint256 bookId) external view returns (string memory);
+    function getBookCover(uint256 bookId) external view returns (string memory);
+    function buyBook(uint256 bookId) external payable;
+    function setBookPrice(uint256 bookId, uint256 newPriceInEth) external;
+
+    // Аукционные функции
+    function startAuction(uint256 bookId, uint256 durationInHours, uint256 startPriceInEth) external;
+    function bid(uint256 bookId) external payable;
+    function auctionEndTime(uint256 bookId) external view returns (uint256);
+    function endAuction(uint256 bookId) external;
+}
