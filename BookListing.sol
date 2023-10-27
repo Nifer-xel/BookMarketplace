c30508d08d32827a6b64a71a274dd8ab35598465
Niferok <64299220+Nifer-xel@users.noreply.github.com>
2023-10-27 00:00:59 -0700

    Update BookListing.sol

--- BookListing.sol
+++ BookListing.sol
@@ -1,70 +1,66 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "./IBookStore.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 
-contract BookListing is ERC721Enumerable, Ownable {
-    using Counters for Counters.Counter;
+contract BookListing is IERC721Receiver {
+    IBookStore public bookStore;
 
-    struct Book {
-        string title;
+    struct Listing {
+        address seller;
         uint256 price;
-        string coverHash;
-        string contentHash;
-        address originalSeller;
-        uint8 royaltyPercentage;
     }
 
-    Counters.Counter private _bookIds;
-    mapping(uint256 => Book) public books;
+    mapping(uint256 => Listing) public listings;
 
-    constructor() ERC721("BookStore", "BOOK") {}
+    event BookListed(uint256 bookId, address seller, uint256 price);
+    event BookDelisted(uint256 bookId);
+    event BookSold(uint256 bookId, address buyer, uint256 price);
 
-    function listBook(
-        string memory title,
-        uint256 priceInEth,
-        string memory coverHash,
-        string memory contentHash,
-        uint8 royaltyPercentage
-    ) external returns (uint256) {
-        uint256 priceInWei = priceInEth * 1e18;
-
-        _bookIds.increment();
-        uint256 newBookId = _bookIds.current();
-
-        _mint(msg.sender, newBookId);
-        books[newBookId] = Book({
-            title: title,
-            price: priceInWei,
-            coverHash: coverHash,
-            contentHash: contentHash,
-            originalSeller: msg.sender,
-            royaltyPercentage: royaltyPercentage
-        });
-
-        return newBookId;
+    constructor(address _bookStore) {
+        bookStore = IBookStore(_bookStore);
     }
 
-    function delist(uint256 bookId) external {
-        require(ownerOf(bookId) == msg.sender, "You are not the owner of this book");
-        books[bookId].price = 0;
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes calldata data
+    ) external pure override returns (bytes4) {
+        return IERC721Receiver.onERC721Received.selector;
     }
 
-    function getBookContent(uint256 bookId) external view returns (string memory) {
-        require(ownerOf(bookId) == msg.sender, "You are not the owner of this book");
-        return books[bookId].contentHash;
+    function listBook(uint256 bookId, uint256 price) external {
+        require(bookStore.ownerOf(bookId) == msg.sender, "You are not the owner of the book");
+        bookStore.safeTransferFrom(msg.sender, address(this), bookId);
+        listings[bookId] = Listing({seller: msg.sender, price: price});
+        emit BookListed(bookId, msg.sender, price);
     }
 
-    function getBookCover(uint256 bookId) external view returns (string memory) {
-        return books[bookId].coverHash;
+    function delistBook(uint256 bookId) external {
+        Listing memory listing = listings[bookId];
+        require(listing.seller == msg.sender, "You are not the seller");
+        bookStore.safeTransferFrom(address(this), listing.seller, bookId);
+        delete listings[bookId];
+        emit BookDelisted(bookId);
     }
 
-    function setBookPrice(uint256 bookId, uint256 newPriceInEth) external {
-        require(ownerOf(bookId) == msg.sender, "You are not the owner of this book");
-
-        uint256 newPriceInWei = newPriceInEth * 1e18;
-        books[bookId].price = newPriceInWei;
+    function buyBook(uint256 bookId) external payable {
+        Listing memory listing = listings[bookId];
+        require(listing.price > 0, "Book not listed");
+        require(msg.value >= listing.price, "Insufficient funds");
+        bookStore.safeTransferFrom(address(this), msg.sender, bookId);
+        uint256 royalty = bookStore.getRoyalty(bookId);
+        uint256 sellerAmount = listing.price - royalty;
+        address originalSeller = bookStore.getOriginalSeller(bookId);
+        payable(originalSeller).transfer(royalty);
+        payable(listing.seller).transfer(sellerAmount);
+        if (msg.value > listing.price) {
+            uint256 change = msg.value - listing.price;
+            payable(msg.sender).transfer(change);
+        }
+        delete listings[bookId];
+        emit BookSold(bookId, msg.sender, listing.price);
     }
 }
